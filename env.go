ad46aaa5083e26ab220ed684b1c53a3016fd543a
Joe Gaetano <jgaetano@gmail.com>
2020-04-19 15:54:13 -0400

    translated country mappings

--- env.go
+++ env.go
@@ -5,81 +5,81 @@ import (
 	"time"
 )
 
-//Env 环境方法
+//Env  .
 var Env env
 
 func init() {
 	//名称
 	Env.nameMap = map[string]string{
-		"ATVPDKIKX0DER":  "美国",   //美
-		"A2EUQ1WTGCTBG2": "加拿大",  //加
-		"A1AM78C64UM0Y8": "墨西哥",  //墨
-		"A2Q3Y263D00KWC": "巴西",   //巴
-		"A2VIGQ35RCS4UG": "阿联酋",  //阿
-		"A1PA6795UKMFR9": "德国",   //德
-		"A1RKKUPIHCS9HS": "西班牙",  //西
-		"A13V1IB3VIYZZH": "法国",   //法
-		"A1F83G8C2ARO7P": "英国",   //英
-		"A21TJRUUN4KGV":  "印度",   //印
-		"APJ6JRA9NG5V4":  "意大利",  //意
-		"A33AVAJ2PDY3EV": "土耳其",  //土
-		"A39IBJ37TRP1C6": "澳大利亚", //澳
-		"A1VC38T7YXB528": "日本",   //日
-		"AAHKV2X7AFYLW":  "中国",   //中
+		"ATVPDKIKX0DER":  "US", // United States
+		"A2EUQ1WTGCTBG2": "CA", // Canada
+		"A1AM78C64UM0Y8": "MX", // Mexico
+		"A2Q3Y263D00KWC": "BR", // Brazil
+		"A2VIGQ35RCS4UG": "AE", // United Arab Emirates
+		"A1PA6795UKMFR9": "DE", // Germany
+		"A1RKKUPIHCS9HS": "ES", // Spain
+		"A13V1IB3VIYZZH": "FR", // France
+		"A1F83G8C2ARO7P": "UK", // United Kingdom
+		"A21TJRUUN4KGV":  "IN", // India
+		"APJ6JRA9NG5V4":  "IT", // Italy
+		"A33AVAJ2PDY3EV": "TU", // Turkey
+		"A39IBJ37TRP1C6": "AU", // Australia
+		"A1VC38T7YXB528": "JP", // Japan
+		"AAHKV2X7AFYLW":  "CN", // China
 	}
 	//域名
 	Env.webDomainMap = map[string]string{
-		"ATVPDKIKX0DER":  "amazon.com",    //美
-		"A2EUQ1WTGCTBG2": "amazon.ca",     //加
-		"A1AM78C64UM0Y8": "amazon.com.mx", //墨
-		"A2Q3Y263D00KWC": "amazon.com.br", //巴
-		"A2VIGQ35RCS4UG": "amazon.ae",     //阿
-		"A1PA6795UKMFR9": "amazon.de",     //德
-		"A1RKKUPIHCS9HS": "amazon.es",     //西
-		"A13V1IB3VIYZZH": "amazon.fr",     //法
-		"A1F83G8C2ARO7P": "amazon.co.uk",  //英
-		"A21TJRUUN4KGV":  "amazon.in",     //印
-		"APJ6JRA9NG5V4":  "amazon.it",     //意
-		"A33AVAJ2PDY3EV": "amazon.com.tr", //土
-		"A39IBJ37TRP1C6": "amazon.com.au", //澳
-		"A1VC38T7YXB528": "amazon.co.jp",  //日
-		"AAHKV2X7AFYLW":  "amazon.cn",     //中
+		"ATVPDKIKX0DER":  "amazon.com",    // US United States
+		"A2EUQ1WTGCTBG2": "amazon.ca",     // CA Canada
+		"A1AM78C64UM0Y8": "amazon.com.mx", // MX Mexico
+		"A2Q3Y263D00KWC": "amazon.com.br", // BR Brazil
+		"A2VIGQ35RCS4UG": "amazon.ae",     // AE United Arab Emirates
+		"A1PA6795UKMFR9": "amazon.de",     // DE Germany
+		"A1RKKUPIHCS9HS": "amazon.es",     // ES Spain
+		"A13V1IB3VIYZZH": "amazon.fr",     // FR France
+		"A1F83G8C2ARO7P": "amazon.co.uk",  // UK United Kingdom
+		"A21TJRUUN4KGV":  "amazon.in",     // IN India
+		"APJ6JRA9NG5V4":  "amazon.it",     // IT Italy
+		"A33AVAJ2PDY3EV": "amazon.com.tr", // TR Turkey
+		"A39IBJ37TRP1C6": "amazon.com.au", // AU Australia
+		"A1VC38T7YXB528": "amazon.co.jp",  // JP Japan
+		"AAHKV2X7AFYLW":  "amazon.cn",     // CN China
 	}
 	//开发者平台域名
 	Env.mwsDomainMap = map[string]string{
-		"ATVPDKIKX0DER":  "mws.amazonservices.com",    //美
-		"A2EUQ1WTGCTBG2": "mws.amazonservices.ca",     //加
-		"A1AM78C64UM0Y8": "mws.amazonservices.com.mx", //墨
-		"A2Q3Y263D00KWC": "mws.amazonservices.com",    //巴
-		"A2VIGQ35RCS4UG": "mws-eu.amazonservices.com", //阿
-		"A1PA6795UKMFR9": "mws-eu.amazonservices.com", //德
-		"A1RKKUPIHCS9HS": "mws-eu.amazonservices.com", //西
-		"A13V1IB3VIYZZH": "mws-eu.amazonservices.com", //法
-		"A1F83G8C2ARO7P": "mws-eu.amazonservices.com", //英
-		"APJ6JRA9NG5V4":  "mws-eu.amazonservices.com", //印
-		"A21TJRUUN4KGV":  "mws.amazonservices.in",     //意
-		"A33AVAJ2PDY3EV": "mws-eu.amazonservices.com", //土
-		"A1VC38T7YXB528": "mws.amazonservices.jp",     //澳
-		"A39IBJ37TRP1C6": "mws.amazonservices.com.au", //日
-		"AAHKV2X7AFYLW":  "mws.amazonservices.com.cn", //中
+		"ATVPDKIKX0DER":  "mws.amazonservices.com",    // US United States
+		"A2EUQ1WTGCTBG2": "mws.amazonservices.ca",     // CA Canada
+		"A1AM78C64UM0Y8": "mws.amazonservices.com.mx", // MX Mexico
+		"A2Q3Y263D00KWC": "mws.amazonservices.com",    // BR Brazil
+		"A2VIGQ35RCS4UG": "mws-eu.amazonservices.com", // AE United Arab Emirates
+		"A1PA6795UKMFR9": "mws-eu.amazonservices.com", // DE Germany
+		"A1RKKUPIHCS9HS": "mws-eu.amazonservices.com", // ES Spain
+		"A13V1IB3VIYZZH": "mws-eu.amazonservices.com", // FR France
+		"A1F83G8C2ARO7P": "mws-eu.amazonservices.com", // UK United Kingdom
+		"APJ6JRA9NG5V4":  "mws-eu.amazonservices.com", // IT Italy
+		"A21TJRUUN4KGV":  "mws.amazonservices.in",     // IN India
+		"A33AVAJ2PDY3EV": "mws-eu.amazonservices.com", // TR Turkey
+		"A1VC38T7YXB528": "mws.amazonservices.jp",     // JP Japan
+		"A39IBJ37TRP1C6": "mws.amazonservices.com.au", // AU Australia
+		"AAHKV2X7AFYLW":  "mws.amazonservices.com.cn", // CN China
 	}
 	//时区 INNA时区
 	Env.tzMap = map[string]string{
-		"ATVPDKIKX0DER":  "America/Los_Angeles", //美 洛杉矶
-		"A2EUQ1WTGCTBG2": "America/Toronto",     //加 多伦多
-		"A1AM78C64UM0Y8": "America/Mexico_City", //墨 墨西哥城
-		"A2Q3Y263D00KWC": "America/Sao_Paulo",   //巴 圣保罗
-		"A2VIGQ35RCS4UG": "Asia/Dubai",          //阿 迪拜
-		"A1PA6795UKMFR9": "Europe/Berlin",       //德 柏林
-		"A1RKKUPIHCS9HS": "Europe/Madrid",       //西 马德里
-		"A13V1IB3VIYZZH": "Europe/Paris",        //法 巴黎
-		"A1F83G8C2ARO7P": "Europe/London",       //英 伦敦
-		"A21TJRUUN4KGV":  "Asia/Kolkata",        //印 加尔各答
-		"APJ6JRA9NG5V4":  "Europe/Rome",         //意 罗马
-		"A33AVAJ2PDY3EV": "Europe/Istanbul",     //土 伊斯坦布尔
-		"A39IBJ37TRP1C6": "Australia/Sydney",    //澳 悉尼
-		"A1VC38T7YXB528": "Asia/Tokyo",          //日 东京
-		"AAHKV2X7AFYLW":  "Asia/Shanghai",       //中 上海
+		"ATVPDKIKX0DER":  "America/Los_Angeles", // US United States
+		"A2EUQ1WTGCTBG2": "America/Toronto",     // CA Canada
+		"A1AM78C64UM0Y8": "America/Mexico_City", // MX Mexico
+		"A2Q3Y263D00KWC": "America/Sao_Paulo",   // BR Brazil
+		"A2VIGQ35RCS4UG": "Asia/Dubai",          // AE United Arab Emirates
+		"A1PA6795UKMFR9": "Europe/Berlin",       // DE Germany
+		"A1RKKUPIHCS9HS": "Europe/Madrid",       // ES Spain
+		"A13V1IB3VIYZZH": "Europe/Paris",        // FR France
+		"A1F83G8C2ARO7P": "Europe/London",       // UK United Kingdom
+		"A21TJRUUN4KGV":  "Asia/Kolkata",        // IN India
+		"APJ6JRA9NG5V4":  "Europe/Rome",         // IT Italy
+		"A33AVAJ2PDY3EV": "Europe/Istanbul",     // TR Turkey
+		"A39IBJ37TRP1C6": "Australia/Sydney",    // AU Australia
+		"A1VC38T7YXB528": "Asia/Tokyo",          // JP Japan
+		"AAHKV2X7AFYLW":  "Asia/Shanghai",       // CN China
 	}
 	//国家分组
 	Env.areaMap = map[string][]string{
@@ -90,21 +90,21 @@ func init() {
 	}
 	//国家关联
 	Env.linkMap = map[string][]string{
-		"ATVPDKIKX0DER":  Env.areaMap["NA"], //美 洛杉矶
-		"A2EUQ1WTGCTBG2": Env.areaMap["NA"], //加 多伦多
-		"A1AM78C64UM0Y8": Env.areaMap["NA"], //墨 墨西哥城
-		"A2Q3Y263D00KWC": Env.areaMap["NA"], //巴 圣保罗
-		"A2VIGQ35RCS4UG": Env.areaMap["EU"], //阿 迪拜
-		"A1PA6795UKMFR9": Env.areaMap["EU"], //德 柏林
-		"A1RKKUPIHCS9HS": Env.areaMap["EU"], //西 马德里
-		"A13V1IB3VIYZZH": Env.areaMap["EU"], //法 巴黎
-		"A1F83G8C2ARO7P": Env.areaMap["EU"], //英 伦敦
-		"A21TJRUUN4KGV":  Env.areaMap["EU"], //印 加尔各答
-		"APJ6JRA9NG5V4":  Env.areaMap["EU"], //意 罗马
-		"A33AVAJ2PDY3EV": Env.areaMap["EU"], //土 伊斯坦布尔
-		"A39IBJ37TRP1C6": Env.areaMap["FE"], //澳 悉尼
-		"A1VC38T7YXB528": Env.areaMap["FE"], //日 东京
-		"AAHKV2X7AFYLW":  Env.areaMap["CN"], //中 上海
+		"ATVPDKIKX0DER":  Env.areaMap["NA"], // US United States
+		"A2EUQ1WTGCTBG2": Env.areaMap["NA"], // CA Canada
+		"A1AM78C64UM0Y8": Env.areaMap["NA"], // MX Mexico
+		"A2Q3Y263D00KWC": Env.areaMap["NA"], // BR Brazil
+		"A2VIGQ35RCS4UG": Env.areaMap["EU"], // AE United Arab Emirates
+		"A1PA6795UKMFR9": Env.areaMap["EU"], // DE Germany
+		"A1RKKUPIHCS9HS": Env.areaMap["EU"], // ES Spain
+		"A13V1IB3VIYZZH": Env.areaMap["EU"], // FR France
+		"A1F83G8C2ARO7P": Env.areaMap["EU"], // UK United Kingdom
+		"A21TJRUUN4KGV":  Env.areaMap["EU"], // IN India
+		"APJ6JRA9NG5V4":  Env.areaMap["EU"], // IT Italy
+		"A33AVAJ2PDY3EV": Env.areaMap["EU"], // TR Turkey
+		"A39IBJ37TRP1C6": Env.areaMap["FE"], // AU Australia
+		"A1VC38T7YXB528": Env.areaMap["FE"], // JP Japan
+		"AAHKV2X7AFYLW":  Env.areaMap["CN"], // CN China
 	}
 }
 
